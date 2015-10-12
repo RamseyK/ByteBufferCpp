eea1ea078247032344163a60deda6b07e00cd566
Ashley Davis <ashley_davis10419@hotmail.com>
2015-10-12 19:51:56 +0100

    Update license header

--- src/ByteBuffer.hpp
+++ src/ByteBuffer.hpp
@@ -14,6 +14,8 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
+
+ Modfied 2015 by Ashley Davis (SgtCoDFish)
  */
 
 #ifndef _ByteBuffer_H_
